e51b3e8e59ae1ff008750fbe1ff8ea9e2940eabf
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-09 20:59:07 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -56,7 +56,7 @@ class Dog
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed).flatten
     if !dog.empty?
-      dog = Dog.new(dog[0], dog[1], dog[2])
+      dog = Dog.new(id:dog[0], name:dog[1], breed:dog[2])
     else
       dog = self.create(name: name, breed: breed)
     end
