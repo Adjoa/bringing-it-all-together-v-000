53b52712e4bc7d68488892107441a49708cf8914
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-09 20:06:24 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -21,11 +21,21 @@ class Dog
   end
 
   def self.drop_table
-    sql=<<-SQL
+    sql = <<-SQL
       DROP TABLE IF EXISTS dogs
     SQL
 
     DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed) 
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
   end
 
   def self.find_or_create_by(name:, album:)
