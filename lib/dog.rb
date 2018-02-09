2b5708a9e6b728675a85f88f2280690b80d83bf8
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-09 20:51:11 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -44,6 +44,14 @@ class Dog
     dog.save
   end
 
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE id = ?
+    SQL 
+
+    DB[:conn].execute(sql, id)
+  end 
+
   def self.find_or_create_by(name:, album:)
   song = DB[:conn].execute("SELECT * FROM songs WHERE name = ? AND album = ?", name, album)
   if !song.empty?
